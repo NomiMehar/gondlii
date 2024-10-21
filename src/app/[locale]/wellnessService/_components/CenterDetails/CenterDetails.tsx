ada9097e5f1c5c6b0eb603258132785f3d784fe2
Muhammad Nouman <59343983+NomiMehar@users.noreply.github.com>
2024-10-21 20:41:04 +0500

    Update CenterDetails.tsx

--- src/app/[locale]/wellnessService/_components/CenterDetails/CenterDetails.tsx
+++ src/app/[locale]/wellnessService/_components/CenterDetails/CenterDetails.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import React, { useState } from 'react';
-import './CenterDetails.scss';
-import Image from 'next/image';
-import Booking from '../Booking/Booking';
-import Reviews from '../../../digitalContent/_components/Reviews/Reviews';
-import Amenities from '../Amenities/Amenities';
-import Location from '../Location/Location';
-import { Modal } from 'react-bootstrap';
-import RecentUpdateModal from '../RecentUpdateModal/RecentUpdateModal';
-import VenueInsights from '../VenueInsights/VenueInsights';
+import React, { useState } from "react";
+import "./CenterDetails.scss";
+import Image from "next/image";
+import Booking from "../Booking/Booking";
+import Reviews from "../../../digitalContent/_components/Reviews/Reviews";
+import Amenities from "../Amenities/Amenities";
+import Location from "../Location/Location";
+import RecentUpdateModal from "../RecentUpdateModal/RecentUpdateModal";
+import VenueInsights from "../VenueInsights/VenueInsights";
 
 const CenterDetails: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(true);
@@ -19,55 +18,55 @@ const CenterDetails: React.FC = () => {
 
   const servicesData = [
     {
-      title: 'Yoga',
-      icon: '/images/services/service1.svg',
+      title: "Yoga",
+      icon: "/images/services/service1.svg",
       items: [
         {
-          name: 'Gentle Flow Yoga Class',
-          duration: '60 Minutes',
-          price: '35.00 CHF',
+          name: "Gentle Flow Yoga Class",
+          duration: "60 Minutes",
+          price: "35.00 CHF",
         },
         {
-          name: 'Gentle Flow Yoga Class',
-          duration: '60 Minutes',
-          price: '35.00 CHF',
+          name: "Gentle Flow Yoga Class",
+          duration: "60 Minutes",
+          price: "35.00 CHF",
         },
         {
-          name: 'Restorative Yoga and Meditation',
-          duration: '90 Minutes',
-          price: '45.00 CHF',
+          name: "Restorative Yoga and Meditation",
+          duration: "90 Minutes",
+          price: "45.00 CHF",
         },
       ],
     },
     {
-      title: 'Spa',
-      icon: '/images/services/service2.svg',
+      title: "Spa",
+      icon: "/images/services/service2.svg",
       items: [
         {
-          name: 'Tranquil Relaxation Massage',
-          duration: '60 Minutes',
-          price: '90.00 CHF',
+          name: "Tranquil Relaxation Massage",
+          duration: "60 Minutes",
+          price: "90.00 CHF",
         },
         {
-          name: 'Revitalizing Aromatherapy Facial',
-          duration: '75 Minutes',
-          price: '110.00 CHF',
+          name: "Revitalizing Aromatherapy Facial",
+          duration: "75 Minutes",
+          price: "110.00 CHF",
         },
       ],
     },
     {
-      title: 'Fitness',
-      icon: '/images/services/service3.svg',
+      title: "Fitness",
+      icon: "/images/services/service3.svg",
       items: [
         {
-          name: 'Energizing Cardio Fitness Class',
-          duration: '45 Minutes',
-          price: '30.00 CHF',
+          name: "Energizing Cardio Fitness Class",
+          duration: "45 Minutes",
+          price: "30.00 CHF",
         },
         {
-          name: 'Mindful Movement Yoga Fusion',
-          duration: '75 Minutes',
-          price: '35.00 CHF',
+          name: "Mindful Movement Yoga Fusion",
+          duration: "75 Minutes",
+          price: "35.00 CHF",
         },
       ],
     },
@@ -100,11 +99,11 @@ const CenterDetails: React.FC = () => {
           <div className="title">
             <h2>Harmony Haven Wellness Center</h2>
             <div className="info">
-              <span className='rating'>
+              <span className="rating">
                 <Image width={12} height={12} src="/images/home/star.svg" alt="star" /> 8.5
               </span>
-              <span className='minute'>$$$$</span>
-              <span className='minute'>44 Minutes</span>
+              <span className="minute">$$$$</span>
+              <span className="minute">44 Minutes</span>
             </div>
           </div>
           <div className="likeUpload">
@@ -126,12 +125,12 @@ const CenterDetails: React.FC = () => {
             <div className="row g-2">
               <div className="col-lg-12 mt-2">
                 <div className="blurBlock">
-                  <Image className='mainImg' src="/images/services/grid1.svg" width={100} height={100} alt="grid1" />
+                  <Image className="mainImg" src="/images/services/grid1.svg" width={100} height={100} alt="grid1" />
                 </div>
               </div>
               <div className="col-lg-6 mt-2">
                 <div className="blurBlock">
-                  <Image className='mainImg' src="/images/services/grid2.svg" width={100} height={100} alt="grid2" />
+                  <Image className="mainImg" src="/images/services/grid2.svg" width={100} height={100} alt="grid2" />
                   <div className="storyContent">
                     <Image src="/images/services/photos.svg" width={100} height={100} alt="photos" />
                     <p>See all photos</p>
@@ -140,7 +139,7 @@ const CenterDetails: React.FC = () => {
               </div>
               <div className="col-lg-6 mt-2">
                 <div className="blurBlock">
-                  <Image className='mainImg' src="/images/services/grid3.svg" width={100} height={100} alt="grid3" />
+                  <Image className="mainImg" src="/images/services/grid3.svg" width={100} height={100} alt="grid3" />
                   <div className="storyContent">
                     <Image src="/images/services/story.svg" width={100} height={100} alt="story" />
                     <p>Check Featured Stories</p>
@@ -155,11 +154,11 @@ const CenterDetails: React.FC = () => {
             <div className="aboutClass">
               <h3>About The Space</h3>
               <p>
-                Welcome to Harmony Haven, where tranquility meets rejuvenation in the heart of Zurich's serene Seefeld district. Nestled in the midst of picturesque landscapes and overlooking the calming waters of Lake Zurich, our wellness center is a sanctuary dedicated to enhancing your overall well-being. At Harmony Haven, {isExpanded ? (
+                Welcome to Harmony Haven, where tranquility meets rejuvenation in the heart of Zurich"s serene Seefeld district. Nestled in the midst of picturesque landscapes and overlooking the calming waters of Lake Zurich, our wellness center is a sanctuary dedicated to enhancing your overall well-being. At Harmony Haven, {isExpanded ? (
                   <>we... </>
-                ) : 'we believe in the power of balance. Welcome to Harmony Haven, where tranquility meets rejuvenation in the heart of Zurichs serene Seefeld district. Nestled in the midst of picturesque landscapes and overlooking the calming waters of Lake Zurich, our wellness center is a sanctuary dedicated to enhancing your overall well-being. '}
+                ) : "we believe in the power of balance. Welcome to Harmony Haven, where tranquility meets rejuvenation in the heart of Zurichs serene Seefeld district. Nestled in the midst of picturesque landscapes and overlooking the calming waters of Lake Zurich, our wellness center is a sanctuary dedicated to enhancing your overall well-being. "}
                 <button className="read-more" onClick={toggleReadMore}>
-                  {isExpanded ? 'Read More' : 'Read Less'}
+                  {isExpanded ? "Read More" : "Read Less"}
                 </button>
               </p>
               <div className="insights">
@@ -168,18 +167,18 @@ const CenterDetails: React.FC = () => {
                     <Image width={40} height={40} src="/images/services/Virta.svg" alt="merck" />
                   </div>
                   <div>
-                    <p className='title'>Hosted By Vitra <Image width={14} height={14} src="/images/services/verify.svg" alt="verify" /></p>
-                    <p className='since'>Member Since 16 Aug, 2023</p>
+                    <p className="title">Hosted By Vitra <Image width={14} height={14} src="/images/services/verify.svg" alt="verify" /></p>
+                    <p className="since">Member Since 16 Aug, 2023</p>
                   </div>
                 </div>
                 <div className="followInsight">
                   <div className="buttons">
                     <button onClick={handleVenueInsightsOpen}>Venue Insights</button>
-                    <button onClick={toggleDropdown}>{isDropdownOpen ? "Following" : "Follow"} <Image src="/images/content/arrowDown.svg" width={8} height={4} alt='arrowDown' /></button>
+                    <button onClick={toggleDropdown}>{isDropdownOpen ? "Following" : "Follow"} <Image src="/images/content/arrowDown.svg" width={8} height={4} alt="arrowDown" /></button>
                     {isDropdownOpen && (
                       <div className="dropdown">
-                        <div className='dropdown-item'> <Image src="/images/content/notifications.svg" width={32} height={32} alt='notifications' />Follow & Receive Notifications</div>
-                        <div className='dropdown-item'> <Image src="/images/content/block.svg" width={32} height={32} alt='block' />Follow Without Notifications</div>
+                        <div className="dropdown-item"> <Image src="/images/content/notifications.svg" width={32} height={32} alt="notifications" />Follow & Receive Notifications</div>
+                        <div className="dropdown-item"> <Image src="/images/content/block.svg" width={32} height={32} alt="block" />Follow Without Notifications</div>
                       </div>
                     )}
                   </div>
